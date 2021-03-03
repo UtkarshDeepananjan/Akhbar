3b152846c227b9ef6683d159c738d5149c51baf6
utkarshdeepanjan <N1mlljysdt!>
2021-03-03 15:38:30 +0530

    working on news widget

--- app/src/main/java/com/uds/akhbar/utils/WidgetWorker.java
+++ app/src/main/java/com/uds/akhbar/utils/WidgetWorker.java
@@ -11,9 +11,7 @@ import com.uds.akhbar.model.Articles;
 import com.uds.akhbar.repository.Repository;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class WidgetWorker extends Worker {
     private List<Articles> mSavedArticles;
@@ -23,23 +21,21 @@ public class WidgetWorker extends Worker {
     public WidgetWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
         mSavedArticles = new ArrayList<>();
-        this.context=context;
+        this.context = context;
     }
 
     @NonNull
     @Override
     public Result doWork() {
         getSavedArticles();
-        Map<String, List<Articles>> listMap = new HashMap<>();
-        listMap.put("articles", mSavedArticles);
-      return Result.success();
+        return Result.success();
     }
 
     public void getSavedArticles() {
         LiveData<List<Articles>> savedArticles = Repository.getInstance().getSavedArticles();
         savedArticles.observeForever(articles ->
         {
-            mSavedArticles=articles;
+            mSavedArticles = articles;
         });
 
     }
